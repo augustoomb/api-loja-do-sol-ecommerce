daf861578e3a6038f5d066f5550b318e105cefc2
Augusto Barbosa <augustobarbosa1988@gmail.com>
2026-07-27 11:45:49 -0300

    first commit

---
+++ src/main/java/com/augustoomb/api_loja_do_sol_ecommerce/ApiLojaDoSolEcommerceApplication.java
@@ -0,0 +1,13 @@
+package com.augustoomb.api_loja_do_sol_ecommerce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiLojaDoSolEcommerceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiLojaDoSolEcommerceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/augustoomb/api_loja_do_sol_ecommerce/ApiLojaDoSolEcommerceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.augustoomb.api_loja_do_sol_ecommerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiLojaDoSolEcommerceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
