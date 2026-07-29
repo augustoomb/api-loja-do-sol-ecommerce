21cf390c1d99df0b25c8a51361b03e8788c2f14f
Augusto Barbosa <augustobarbosa1988@gmail.com>
2026-07-29 17:51:18 -0300

    fix: corrigindo importaçao CascadeType

--- src/main/java/com/augustoomb/api_loja_do_sol_ecommerce/model/User.java
+++ src/main/java/com/augustoomb/api_loja_do_sol_ecommerce/model/User.java
@@ -4,6 +4,7 @@ import java.time.LocalDateTime;
 import java.util.HashSet;
 import java.util.Set;
 
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.FetchType;
