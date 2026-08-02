c76ebef94b8c9bd465fad541eed0825deaf6d2d3
Augusto Barbosa <augustobarbosa1988@gmail.com>
2026-08-02 12:13:08 -0300

    feat: ajustando acesso a rotas: somente admin nas rotas criticas

--- src/main/java/com/augustoomb/api_loja_do_sol_ecommerce/config/SecurityConfig.java
+++ src/main/java/com/augustoomb/api_loja_do_sol_ecommerce/config/SecurityConfig.java
@@ -29,10 +29,11 @@ public class SecurityConfig {
                 .csrf(csrf -> csrf.disable()) // DESABILITA POIS SO E UTIL EM APLICAÇOES QUE USAM COOKIES DE NAVEGADOR
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS)) // API REST não mantém sessão no servidor
                 .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/api/auth/login").permitAll()
+                        .requestMatchers("/api/auth/login").permitAll() // Login é público
                         .requestMatchers(HttpMethod.POST, "/api/users").permitAll() // Cadastro de usuário é público
-                        .requestMatchers("/v3/api-docs/**", "/swagger-ui/**", "/swagger-ui.html").permitAll()
-                        .anyRequest().authenticated())
+                        .requestMatchers("/v3/api-docs/**", "/swagger-ui/**", "/swagger-ui.html").permitAll() // Docs públicas
+                        .requestMatchers(HttpMethod.GET, "/api/products/**", "/api/categories/**").authenticated() // Leitura de produtos/categorias: ADMIN e USER
+                        .anyRequest().hasRole("ADMIN")) // Restante: somente ADMIN
                 .formLogin(form -> form.disable()) // Desativa a página de login em HTML gerada automaticamente pelo Spring Security.
                 .httpBasic(basic -> basic.disable()) // Desativa a autenticação do tipo HTTP Basic (aquela janela nativa do navegador que pede usuário e senha via cabeçalho HTTP).
                 .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class);
